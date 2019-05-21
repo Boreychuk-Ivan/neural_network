ea3c90ab1c4b444fb8cf5e40029d4b91a9a6504b
Boreychuk-Ivan <47390744+Boreychuk-Ivan@users.noreply.github.com>
2019-05-21 12:53:12 +0300

    Добавлен рассчет коррекции весов

--- neural_network/neural_network_lib/include/back_propagation.h
+++ neural_network/neural_network_lib/include/back_propagation.h
@@ -7,19 +7,22 @@ class BackPropagation
 {
 private:
     NeuralNetwork m_neural_network;
-    Matrix<double> m_delta_weights;
 public:
-    Vector<double> CalculateError(const Vector<double> kOutputValues, const Vector<double> kTargetValues);
+    Vector<double> CalculateError(const Vector<double>& kOutputValues, const Vector<double>& kTargetValues);
     
     Vector<double> CalculateLocalGradients(
-        const Vector<double> kDerivativeValues, 
-        const Vector<double> kPreviousLayerError, 
-        const Matrix<double> kSynapticWeights
+        const Vector<double>& kDerivativeValues, 
+        const Vector<double>& kPreviousLayerError, 
+        const Matrix<double>& kSynapticWeights
     );
 
     Matrix<double> CalculateDeltaWeights(
-        const double kLearningRate, 
-        const Vector<double> kLocalGradients, 
-        const Vector<double> kInputValues
+        const double& kLearningRate, 
+        const double& kMomentum,
+        const Vector<double>& kLocalGradients, 
+        const Vector<double>& kInputValues,
+        const Matrix<double>& kLastDeltaWeigths
     );
+
+    void AdjustmentWeight();
 };
--- neural_network/neural_network_lib/src/back_propagation.cpp
+++ neural_network/neural_network_lib/src/back_propagation.cpp
@@ -1,23 +1,27 @@
 #include "back_propagation.h"
 
-Vector<double> BackPropagation::CalculateError(const Vector<double> kOutputValues, const Vector<double> kTargetValues)
+Vector<double> BackPropagation::CalculateError(
+    const Vector<double>& kOutputValues, 
+    const Vector<double>& kTargetValues)
 {
     assert(kOutputValues.IsEqualSize(kTargetValues));
     return Vector<double>(kTargetValues - kOutputValues);
 }
 
 Vector<double> BackPropagation::CalculateLocalGradients(
-    const Vector<double> kDerivativeValues, 
-    const Vector<double> kPreviousLayerError, 
-    const Matrix<double> kSynapticWeights)
+    const Vector<double>& kDerivativeValues, 
+    const Vector<double>& kPreviousLayerError, 
+    const Matrix<double>& kSynapticWeights)
 {
     return Vector<double>(kDerivativeValues.dot(kPreviousLayerError * kSynapticWeights));
 }
 
 Matrix<double> BackPropagation::CalculateDeltaWeights(
-    const double kLearningRate, 
-    const Vector<double> kLocalGradients, 
-    const Vector<double> kInputValues)
+    const double& kLearningRate, 
+    const double& kMomentum, 
+    const Vector<double>& kLocalGradients, 
+    const Vector<double>& kInputValues, 
+    const Matrix<double>& kLastDeltaWeigths)
 {
-    
+    return (kMomentum * kLastDeltaWeigths + kLearningRate * !kLocalGradients * kInputValues); 
 }
